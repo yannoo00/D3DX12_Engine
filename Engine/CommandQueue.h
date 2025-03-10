d383a0c91ed34f4bfd5947c0c5db6e84037c5156
yannoo00 <yannoo@naver.com>
2025-03-10 21:47:19 +0900

    Texture Mapping

--- Engine/CommandQueue.h
+++ Engine/CommandQueue.h
@@ -17,16 +17,23 @@ public:
 	void RenderBegin(const D3D12_VIEWPORT* vp, const D3D12_RECT* rect);
 	void RenderEnd();
 
+	void FlushResourceCommandQueue();
+
 	ComPtr<ID3D12CommandQueue> GetCmdQueue() { return _cmdQueue; }
 	ComPtr<ID3D12GraphicsCommandList> GetCmdList() { return _cmdList; }
+	ComPtr<ID3D12GraphicsCommandList> GetResourceCmdList() { return _resCmdList; }
 
 private:
 	// CommandQueue : DX12에 등장
 	// 외주를 요청할 때, 하나씩 요청하면 비효율적
 	// [외주 목록]에 일감을 차곡차곡 기록했다가 한 방에 요청하는 것
 	ComPtr<ID3D12CommandQueue>			_cmdQueue;
+
 	ComPtr<ID3D12CommandAllocator>		_cmdAlloc;
 	ComPtr<ID3D12GraphicsCommandList>	_cmdList;
+
+	ComPtr<ID3D12CommandAllocator> _resCmdAlloc;
+	ComPtr<ID3D12GraphicsCommandList> _resCmdList;
 
 	// Fence : 울타리(?)
 	// CPU / GPU 동기화를 위한 간단한 도구
--- Engine/EnginePch.h
+++ Engine/EnginePch.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// std::byte 사용하지 않음
+#define _HAS_STD_BYTE 0
+
+// 각종 include
 #include <windows.h>
 #include <tchar.h>
 #include <memory>
@@ -9,6 +13,9 @@
 #include <list>
 #include <map>
 using namespace std;
+
+#include <filesystem>
+namespace fs = std::filesystem;
 
 #include "d3dx12.h"
 #include <d3d12.h>
@@ -22,15 +29,22 @@ using namespace DirectX;
 using namespace DirectX::PackedVector;
 using namespace Microsoft::WRL;
 
+#include <DirectXTex/DirectXTex.h>
+#include <DirectXTex/DirectXTex.inl>
 
-//각종 lib
+// 각종 lib
 #pragma comment(lib, "d3d12")
 #pragma comment(lib, "dxgi")
 #pragma comment(lib, "dxguid")
 #pragma comment(lib, "d3dcompiler")
 
+#ifdef _DEBUG
+#pragma comment(lib, "DirectXTex\\DirectXTex_debug.lib")
+#else
+#pragma comment(lib, "DirectXTex\\DirectXTex.lib")
+#endif
 
-//각종 typedef
+// 각종 typedef
 using int8 = __int8;
 using int16 = __int16;
 using int32 = __int32;
@@ -44,7 +58,7 @@ using Vec3 = XMFLOAT3;
 using Vec4 = XMFLOAT4;
 using Matrix = XMMATRIX;
 
-enum class CBV_REGISTER
+enum class CBV_REGISTER : uint8
 {
 	b0,
 	b1,
@@ -52,28 +66,41 @@ enum class CBV_REGISTER
 	b3,
 	b4,
 
-	END //END로 몇 개인지 관리
+	END
+};
+
+enum class SRV_REGISTER : uint8
+{
+	t0 = static_cast<uint8>(CBV_REGISTER::END),
+	t1,
+	t2,
+	t3,
+	t4,
+
+	END
 };
 
 enum
 {
 	SWAP_CHAIN_BUFFER_COUNT = 2,
 	CBV_REGISTER_COUNT = CBV_REGISTER::END,
-	REGISTER_COUNT = CBV_REGISTER::END,
+	SRV_REGISTER_COUNT = static_cast<uint8>(SRV_REGISTER::END) - CBV_REGISTER_COUNT,
+	REGISTER_COUNT = CBV_REGISTER_COUNT + SRV_REGISTER_COUNT,
 };
 
 struct WindowInfo
 {
-	HWND	hwnd;
-	int32	width;
-	int32	height;
-	bool	windowed;
+	HWND	hwnd; // 출력 윈도우
+	int32	width; // 너비
+	int32	height; // 높이
+	bool	windowed; // 창모드 or 전체화면
 };
 
 struct Vertex
 {
 	Vec3 pos;
 	Vec4 color;
+	Vec2 uv;
 };
 
 struct Transform
@@ -81,9 +108,9 @@ struct Transform
 	Vec4 offset;
 };
 
-#define DEVICE			GEngine->GetDevice() -> GetDevice()
-#define CMD_LIST		GEngine->GetCmdQueue() -> GetCmdList()
-#define ROOT_SIGNATURE	GEngine->GetRootSignature() -> GetSignature()
+#define DEVICE				GEngine->GetDevice()->GetDevice()
+#define CMD_LIST			GEngine->GetCmdQueue()->GetCmdList()
+#define RESOURCE_CMD_LIST	GEngine->GetCmdQueue()->GetResourceCmdList()
+#define ROOT_SIGNATURE		GEngine->GetRootSignature()->GetSignature()
 
-class Engine;
-extern unique_ptr<Engine> GEngine;
+extern unique_ptr<class Engine> GEngine;
