dc7a6a73c27e032796975901f1d610040cb0da7f
yannoo00 <yannoo@naver.com>
2025-03-12 19:50:38 +0900

    depth(stencil) buffer

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -7,8 +7,6 @@ void Engine::Init(const WindowInfo& info)
 {
 	_window = info;
 
-	ResizeWindow(info.width, info.height);
-
 	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
 	_scissorRect = CD3DX12_RECT(0, 0, info.width, info.height);
 
@@ -18,6 +16,7 @@ void Engine::Init(const WindowInfo& info)
 	_rootSignature = make_shared<RootSignature>();
 	_cb = make_shared<ConstantBuffer>();
 	_tableDescHeap = make_shared<TableDescriptorHeap>();
+	_depthStencilBuffer = make_shared<DepthStencilBuffer>();
 
 	_device->Init();
 	_cmdQueue->Init(_device->GetDevice(), _swapChain);
@@ -25,6 +24,9 @@ void Engine::Init(const WindowInfo& info)
 	_rootSignature->Init();
 	_cb->Init(sizeof(Transform), 256);
 	_tableDescHeap->Init(256);
+	_depthStencilBuffer->Init(_window);
+
+	ResizeWindow(info.width, info.height);
 }
 
 void Engine::Render()
@@ -53,4 +55,5 @@ void Engine::ResizeWindow(int32 width, int32 height)
 	::AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
 	::SetWindowPos(_window.hwnd, 0, 100, 100, width, height, 0);
 
+	_depthStencilBuffer->Init(_window);
 }
