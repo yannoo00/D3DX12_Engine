c1ca72ccef0a202ea2d5d863570783f136565d23
yannoo00 <yannoo@naver.com>
2025-04-12 21:48:28 +0900

    Instancing

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -7,6 +7,9 @@
 #include "SceneManager.h"
 #include "Light.h"
 #include "Resources.h"
+#include "InstancingManager.h"
+
+
 
 void Engine::Init(const WindowInfo& info)
 {
@@ -42,6 +45,7 @@ void Engine::Update()
 	GET_SINGLE(Input)->Update();
 	GET_SINGLE(Timer)->Update();
 	GET_SINGLE(SceneManager)->Update();
+	GET_SINGLE(InstancingManager)->ClearBuffer();
 
 	Render();
 
--- Engine/Object.h
+++ Engine/Object.h
@@ -29,6 +29,8 @@ public:
 	void SetName(const wstring& name) { _name = name; }
 	const wstring& GetName() { return _name; }
 
+	uint32 GetID() { return _id; }
+
 	// TODO : Instantiate
 
 protected:
@@ -39,5 +41,7 @@ protected:
 protected:
 	OBJECT_TYPE _objectType = OBJECT_TYPE::NONE;
 	wstring _name;
+
+	uint32 _id = 0;
 };
 
