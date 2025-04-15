fdc04231783d1cf7bdae5891007fc477a654701d
yannoo00 <yannoo@naver.com>
2025-04-15 22:50:53 +0900

    Shadow Mapping

--- Engine/Transform.h
+++ Engine/Transform.h
@@ -16,8 +16,8 @@ public:
 	const Vec3& GetLocalRotation() { return _localRotation; }
 	const Vec3& GetLocalScale() { return _localScale; }
 
-	//TEMP
-	float GetBoundingSphereRadius() { return max(max(_localScale.x, _localScale.y), _localScale.z);  }
+	// TEMP
+	float GetBoundingSphereRadius() { return max(max(_localScale.x, _localScale.y), _localScale.z); }
 
 	const Matrix& GetLocalToWorldMatrix() { return _matWorld; }
 	Vec3 GetWorldPosition() { return _matWorld.Translation(); }
@@ -29,6 +29,11 @@ public:
 	void SetLocalPosition(const Vec3& position) { _localPosition = position; }
 	void SetLocalRotation(const Vec3& rotation) { _localRotation = rotation; }
 	void SetLocalScale(const Vec3& scale) { _localScale = scale; }
+
+	void LookAt(const Vec3& dir);
+
+	static bool CloseEnough(const float& a, const float& b, const float& epsilon = std::numeric_limits<float>::epsilon());
+	static Vec3 DecomposeRotationMatrix(const Matrix& rotation);
 
 public:
 	void SetParent(shared_ptr<Transform> parent) { _parent = parent; }
